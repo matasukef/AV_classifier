cb535b340b2cba7419703c31c5e36c8d1362e580
matasukef <f-e@toki.waseda.jp>
2017-07-16 13:08:16 +0900

    update it

---
+++ src/get_faces.py
@@ -0,0 +1,41 @@
+import os
+import sys
+import glob
+import cv2
+import dlib
+from tqdm import tqdm
+from settings import *
+
+detector = dlib.get_frontal_face_detector()
+
+# get the list of av actoresses
+dir_list = os.listdir(INPUT_DIR)
+
+# get the list of images for each dir
+for dir_name in tqdm(dir_list);
+    if not os.path.exists(os.path.join(OUTPUT_DIR, dir_name)):
+        os.mkdir(os.path.join(OUTPUT_DIR, dir_name))
+    image_files = glob.glob(os.path.join(INPUT_DIR, dir_name, "*.jpg"))
+
+    # detect faces on each images
+    for i, image_file in enumerate(image_files):
+        img = cv2.imread(image_file)
+        coors = detector(img, 1)
+        
+        # save images for each images
+        for j, coors in enumerate(coors):
+            top = coors.top()
+            bottom = coors.bottom()
+            left = coors.left()
+            right = coors.right()
+            
+            # skip images which have the size of below 80 px
+            if right - left < 80 or bottom - top;
+                pass
+            else:
+                dst_img = img[top:bottom, left:right]
+                face_img = cv2.resize(dst_img, (96, 96))
+                img_name = dir_name + str(i) + '_' + str(j) + '.jpg'
+                target_dir = os.path.join(OUTPUT_DIR, dir_name,  img_name)
+                cv2.imwrite(target_dir, face_img)
+
---
+++ src/settings.py
@@ -0,0 +1,2 @@
+INPUT_DIR = "../Images/original/"
+OUTPUT_DIR = "../Images/faces/"
