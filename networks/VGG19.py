5e3d12dc7b0aac66f3bc1dbdcf2c8a447af60b99
matasukef <f-e@toki.waseda.jp>
2017-07-21 22:34:01 +0900

    update networks

--- networks/VGG19.py
+++ networks/VGG19.py
@@ -1,4 +1,6 @@
 import os
+import sys
+sys.path.append('../setup')
 from keras.applications.vgg19 import VGG19
 from keras.applications.vgg19 import preprocess_input
 from keras.models import Model
@@ -6,39 +8,41 @@ from keras.models import Sequential
 from keras.layers import Dense, Dropout, Activation, Flatten, Input
 from keras.layers import Convolution2D, MaxPooling2D
 from keras.preprocessing.image import ImageDataGenerator
+from keras import optimizers
 from keras.callbacks import EarlyStopping, ModelCheckpoint
 from settings import *
 import numpy as np
 import glob
 
 # get the name of each classes
-class_list = os.listdir(os.path.join(OUTPUT_DIR))
+classes = os.listdir(TRAIN_DIR)
 
-batch_size = 128
-nb_claas = len(class_list)
+batch_size = 64
+nb_class = len(classes)
 data_augmentation = True
 
 # input image dimensions
 img_rows, img_cols, channels = (96, 96, 3)
 input_shape = (img_rows, img_cols, channels)
 
-train_data_dir = 'train_images'
-validation_data_dir = 'test_images'
+train_data_dir = TRAIN_DIR
+validation_data_dir = VALID_DIR
+
+nb_train_samples = nb_class * 16
+nb_val_samples = nb_class * 4
+nb_epoch = 100
 
 result_dir = os.path.join(HOME_DIR, 'results')
 if not os.path.exists(result_dir):
     os.mkdir(result_dir)
 
-def calc_mean(img_dir):
-
 
 def preprocess(x):
     x = x[:, :, ::-1]
-    X[:, :, 0] -= 103.939
-    X[:, :, 1] -= 116.779
-    X[:. :, 2] -= 123.68
+    x[:, :, 0] -= 103.939
+    x[:, :, 1] -= 116.779
+    x[:, :, 2] -= 123.68
     return x
-
 
 
 def save_history(history, result_file):
@@ -52,6 +56,7 @@ def save_history(history, result_file):
         f.write('epoch\tloss\tacc\tval_loss\tval_acc\n\n')
         for i in range(nb_epoch):
             f.write('{0}\t{1}\t{2}\t{3}\t{4}'.format(i, loss[i], acc[i], val_loss[i], val_acc[i]))
+
 
 if __name__ == "__main__":
 
@@ -78,14 +83,16 @@ if __name__ == "__main__":
 
     train_datagen = ImageDataGenerator(
             preprocessing_function=preprocess,
-            rescale=1.0/255,
-            sheer_range=0.2,
+            #rescale=1.0/255,
+            shear_range=0.2,
             zoom_range=0.2,
-            horizontal_flip=True)
+            horizontal_flip=True
+    )
 
     test_datagen = ImageDataGenerator(
-            preprocessing_function=preprocess,
-            rescale=1.0/255)
+            #rescale=1.0/255,
+            preprocessing_function=preprocess
+    )
 
     train_generator = train_datagen.flow_from_directory(
             train_data_dir,
@@ -106,4 +113,20 @@ if __name__ == "__main__":
             batch_size = batch_size,
             shuffle = True
     )
+
+    # save weight on the way
+    checkpointer = ModelCheckpoint(os.path.join(result_dir, 'mid', 'mid_weights.h5'), verbose=1, save_best_only=True)
+    early_stopping = EarlyStopping(monitor='val_loss', patience=10)
+
+    history = model.fit_generator(
+        train_generator,
+        samples_per_epoch = nb_train_samples,
+        nb_epoch = nb_epoch,
+        validation_data = validation_generator,
+        nb_val_samples = nb_val_samples,
+        callbacks = [checkpointer, early_stopping]
+    )
+
+    model.save_weights(os.path.join(result_dir, report, 'finetuning.h5'))
+    save_history(history, os.path.join(result_dir, 'history_finetuning.txt'))
 
